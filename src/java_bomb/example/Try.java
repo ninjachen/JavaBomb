7df75f543e9a1b856e85f6f6d6bc9504d820a03e
jiachenning <jiachenning@gmail.com>
2014-03-28 01:16:07 +0800

    fix try example's syntax error. change the file to idea workspace(project)

--- src/java_bomb/example/Try.java
+++ src/java_bomb/example/Try.java
@@ -6,15 +6,18 @@ package java_bomb.example;
  * @author ninja
  */
 public class Try {
+    //the sequence of execute order is quite strange,
+    // Run finally first ,then return.
+    // But the return value is previous, the finally value is the finally
 	public static void main(String[] args) {
-	System.out.println("-----------after return,result is"+testFinally(+"---------"));
+	System.out.println("-----------in main   ,result is" + testFinally() + "----------------");
 	}
 	public static int testFinally(){
 	int result = 0;
 	try{
 	return result = 999+101;
 	}finally{
-	System.out.println("-----------before return,result is" + result + "----------------");
+	System.out.println("-----------in finally,result is" + ++result + "----------------");
 	}
 	}
 }
