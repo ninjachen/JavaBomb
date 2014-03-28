af9545a1e2719948a6b893b412c69986ce8e7aa8
jiachenning <jiachenning@gmail.com>
2014-03-28 16:46:30 +0800

    add DiamondOperator

---
+++ src/java_bomb/example/DiamondOperator.java
@@ -0,0 +1,27 @@
+package java_bomb.example;
+
+
+/**
+ * try to describe my worry
+ * @author ninja
+ */
+public class DiamondOperator {
+    //the DiamondOperator is weak
+	public static void main(String[] args) {
+	}
+
+	public static int testDiamondOperator(){
+	List<String> list = new ArrayList<>();//this is a diamond operator,new feature in JDK7
+
+	//in {$JDK_VERSION} < 7,YOU MUST GIVE TYPE TWICE
+	List<String> list_oldStyle = new ArrayList<String>();
+
+	list.add("a string");//this is ok
+
+	//this will occur compile error
+	list.addAll(new ArrayList<>());
+	//since addAll() expects Collection<? extends String>
+	
+	list.addAll(new ArrayList<String>());
+
+}
